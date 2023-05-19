71261cf58ff6357e9d6caf0e82f93b4cd3620fac
luis.cardenasv <luis.cardenasv@coppel.com>
2023-05-19 11:17:08 -0700

    se cambia logica de mostrar el tiempo de las runs
    se agrego milisegundos a la logica y se quito los minutos cuando estos son 0 y hay mas de 0 horas en la run

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,42 @@
-export function formatTime(seconds) {
-    let output = 
-            (seconds / 3600 >= 1 ? Math.floor(seconds / 3600) + "h " : "")
-            + Math.floor(seconds / 60 % 60) + "m "
-            + seconds % 60 + "s";
-    return output;
+export function formatTime(totalSeconds) {
+    let hours = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor(totalSeconds / 60 % 60);
+    let seconds = Math.floor(totalSeconds % 60);
+    let milliseconds = totalSeconds % 1;
+
+    if(hours >= 1) {
+        hours+= "h ";
+        minutes = setMinutes(minutes);
+        seconds = setSeconds(seconds);
+        milliseconds = setMilliseconds(milliseconds);
+    }
+    else {
+        hours = "";
+        if(minutes >= 1)
+            minutes+= "m ";
+        else
+            minutes = "";
+        seconds = setSeconds(seconds);
+        milliseconds = setMilliseconds(milliseconds);
+    }
+    
+    return hours + minutes + seconds + milliseconds;;
+}
+
+function setMinutes(minutes) {
+    if(minutes == 0)
+        return "";
+    return minutes + "m ";
+}
+
+function setSeconds(seconds) {
+    if(seconds == 0)
+        return "";
+    return seconds + "s";
+}
+
+function setMilliseconds(milliseconds) {
+    if(milliseconds == 0)
+        return "";
+    return ` ${Math.round(milliseconds*1000)}ms`;
 }
